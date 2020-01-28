86e936cea1a975a90d6446479cb43302fe9d89d0
Darion Suggs <dariontsuggs@icloud.com>
2020-01-27 19:41:23 -0600

    first commit

---
+++ build/precache-manifest.77f22a15c7b60a4999ce9ec92c9ff7e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa08f4b9dd614837d6681631dbd3d886",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a432a9ee96c19811847c",
+    "url": "/static/js/2.e246a7d1.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.e246a7d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d315834a5b45ef867e1a",
+    "url": "/static/js/main.ecf38319.chunk.js"
+  },
+  {
+    "revision": "1503d9b3e611c5bd3832",
+    "url": "/static/js/runtime-main.a7f4b282.js"
+  }
+]);
